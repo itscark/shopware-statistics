7e2acec7e99fa450f697fd3f1cc61e4fa57ececd
Marc Peternell <marcpeternell@me.com>
2021-12-10 22:02:12 +0100

    feat: add dead message search

--- src/Resources/app/administration/src/module/task-statistics-dead-message/page/task-statistics-dead-message/index.js
+++ src/Resources/app/administration/src/module/task-statistics-dead-message/page/task-statistics-dead-message/index.js
@@ -18,6 +18,7 @@ Component.register('task-statistics-dead-message', {
             items: null,
             repository: null,
             isLoading: false,
+            term: '',
         };
     },
 
@@ -30,37 +31,95 @@ Component.register('task-statistics-dead-message', {
     computed: {
         columns() {
             return [
-                { property: 'id', label: this.$tc('task-statistics-dead-message.grid.column.id') },
-                { property: 'originalMessageClass', label: this.$tc('task-statistics-dead-message.grid.column.originalMessageClass') },
-                { property: 'serializedOriginalMessage', label: this.$tc('task-statistics-dead-message.grid.column.serializedOriginalMessage') },
-                { property: 'handlerClass', label: this.$tc('task-statistics-dead-message.grid.column.handlerClass') },
-                { property: 'encrypted', label: this.$tc('task-statistics-dead-message.grid.column.encrypted') },
-                { property: 'errorCount', label: this.$tc('task-statistics-dead-message.grid.column.errorCount') },
-                { property: 'nextExecutionTime', label: this.$tc('task-statistics-dead-message.grid.column.nextExecutionTime') },
-                { property: 'exception', label: this.$tc('task-statistics-dead-message.grid.column.exception') },
-                { property: 'exceptionMessage', label: this.$tc('task-statistics-dead-message.grid.column.exceptionMessage') },
-                { property: 'exceptionFile', label: this.$tc('task-statistics-dead-message.grid.column.exceptionFile') },
-                { property: 'exceptionLine', label: this.$tc('task-statistics-dead-message.grid.column.exceptionLine') },
-                { property: 'scheduledTaskId', label: this.$tc('task-statistics-dead-message.grid.column.scheduledTaskId') },
-                { property: 'createdAt', label: this.$tc('task-statistics-dead-message.grid.column.createdAt') },
-                { property: 'updatedAt', label: this.$tc('task-statistics-dead-message.grid.column.updatedAt') },
+                {
+                    property: 'id',
+                    label: this.$tc('task-statistics-dead-message.grid.column.id')
+                },
+                {
+                    property: 'originalMessageClass',
+                    label: this.$tc('task-statistics-dead-message.grid.column.originalMessageClass')
+                },
+                {
+                    property: 'serializedOriginalMessage',
+                    label: this.$tc('task-statistics-dead-message.grid.column.serializedOriginalMessage')
+                },
+                {
+                    property: 'handlerClass',
+                    label: this.$tc('task-statistics-dead-message.grid.column.handlerClass')
+                },
+                {
+                    property: 'encrypted',
+                    label: this.$tc('task-statistics-dead-message.grid.column.encrypted')
+                },
+                {
+                    property: 'errorCount',
+                    label: this.$tc('task-statistics-dead-message.grid.column.errorCount')
+                },
+                {
+                    property: 'nextExecutionTime',
+                    label: this.$tc('task-statistics-dead-message.grid.column.nextExecutionTime')
+                },
+                {
+                    property: 'exception',
+                    label: this.$tc('task-statistics-dead-message.grid.column.exception')
+                },
+                {
+                    property: 'exceptionMessage',
+                    label: this.$tc('task-statistics-dead-message.grid.column.exceptionMessage')
+                },
+                {
+                    property: 'exceptionFile',
+                    label: this.$tc('task-statistics-dead-message.grid.column.exceptionFile')
+                },
+                {
+                    property: 'exceptionLine',
+                    label: this.$tc('task-statistics-dead-message.grid.column.exceptionLine')
+                },
+                {
+                    property: 'scheduledTaskId',
+                    label: this.$tc('task-statistics-dead-message.grid.column.scheduledTaskId')
+                },
+                {
+                    property: 'createdAt',
+                    label: this.$tc('task-statistics-dead-message.grid.column.createdAt')
+                },
+                {
+                    property: 'updatedAt',
+                    label: this.$tc('task-statistics-dead-message.grid.column.updatedAt')
+                },
             ];
         },
 
         entityRepository() {
             return this.repositoryFactory.create('dead_message');
         },
-    },
 
-    methods: {
-        getList() {
-            this.isLoading = true;
+        listingCriteria() {
+
+            if (this.term) {
+                const criteria = new Criteria();
+                criteria.addFilter(Criteria.contains('originalMessageClass', this.term));
+                criteria.addSorting(
+                    Criteria.sort('createdAt', 'DESC'),
+                );
+                console.log('term not empty', criteria);
+                return criteria;
+            }
+
             const criteria = new Criteria();
             criteria.addSorting(
                 Criteria.sort('createdAt', 'DESC'),
             );
 
-            this.entityRepository.search(criteria, Context.api).then(result => {
+            return criteria;
+        }
+    },
+
+    methods: {
+        getList() {
+            this.isLoading = true;
+
+            this.entityRepository.search(this.listingCriteria, Context.api).then(result => {
                 this.items = result;
                 this.total = result.total;
                 this.isLoading = false;
